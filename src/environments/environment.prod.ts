de613c4a96e6fa6ff37389a762b394a86be3417b
Ariel <ariel.f.alaniz.frias@gmail.com>
2020-01-05 14:45:59 -0300

    Update cambio de environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyByIygaP8GInbNx4IBhH9ld5PnLlGvrHlE",
-    authDomain: "listacompras-1b933.firebaseapp.com",
-    databaseURL: "https://listacompras-1b933.firebaseio.com",
-    projectId: "listacompras-1b933",
-    storageBucket: "",
-    messagingSenderId: "54129444258"
+    apiKey: "AIzaSyAdfT-96Ze5gUT98ylngMJFy6lBp_3kz3w",
+    authDomain: "app-alaniz.firebaseapp.com",
+    databaseURL: "https://app-alaniz.firebaseio.com",
+    projectId: "app-alaniz",
+    storageBucket: "app-alaniz.appspot.com",
+    messagingSenderId: "906609449444"
+    // appID: "app-id"
   }
 };
